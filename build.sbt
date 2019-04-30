4b2a3938516bad9c4145a74156444bc8ba53c54e
maria ra <mariia.rabyk@gmail.com>
2019-04-30 10:02:17 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Algorithms"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+organization := "software.sigma"
+
+libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "3.2.0-SNAP9" % Test)
---
+++ src/test/scala/ua/com/sorting/SelectionSortTest.scala
@@ -0,0 +1,46 @@
+package ua.com.sorting
+
+import org.scalatest.FunSuite
+
+class SelectionSortTest extends FunSuite {
+
+  val ints = List(4, 56, 90, 13, 11, 2, 57, 88, 33)
+  val chars = List('s', 'v', 'a', 'b', 'd')
+  val doubles = List(1.56, 45.03, 99.99, 0.33, 11.13, 5.005, 5.006, 23.05)
+
+  test("Insertion sorting can sort list of integers") {
+    val expected = List(2, 4, 11, 13, 33, 56, 57, 88, 90)
+
+    val sortedOne = SelectionSort.sortOne(ints)
+    val sortedTwo = SelectionSort.sortTwo(ints)
+    assert(sortedOne == expected)
+    assert(sortedTwo == expected)
+    assert(sortedOne.head == 2)
+    assert(sortedTwo.last == 90)
+  }
+
+  test("Insertion sorting can sort list of characters") {
+    val expected = List('a', 'b', 'd', 's', 'v')
+
+    val sortedOne = SelectionSort.sortOne(chars)
+    val sortedTwo = SelectionSort.sortTwo(chars)
+
+    assert(sortedOne == expected)
+    assert(sortedTwo == expected)
+    assert(sortedOne.head == 'a')
+    assert(sortedTwo.last == 'v')
+  }
+
+  test("Insertion sorting can sort list of doubles") {
+    val expected = List(0.33, 1.56, 5.005, 5.006, 11.13, 23.05, 45.03, 99.99)
+
+    val sortedOne = SelectionSort.sortOne(doubles)
+    val sortedTwo = SelectionSort.sortTwo(doubles)
+
+    assert(sortedOne == expected)
+    assert(sortedTwo == expected)
+    assert(sortedOne.head == 0.33)
+    assert(sortedTwo.last == 99.99)
+  }
+
+}
