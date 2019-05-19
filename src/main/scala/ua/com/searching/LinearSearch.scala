ca4a738f8e9c808183c21295003604446c5570bf
maria ra <mariia.rabyk@gmail.com>
2019-05-19 12:55:38 +0300

    Added linear search

---
+++ src/main/scala/ua/com/searching/LinearSearch.scala
@@ -0,0 +1,23 @@
+package ua.com.searching
+
+import scala.annotation.tailrec
+
+object LinearSearch {
+
+  def search[T](elements: List[T], value: T)(implicit ord: Ordering[T]): Int = {
+
+    @tailrec
+    def searchElement[T](list: List[T], value: T)(implicit ord: Ordering[T]): Int = {
+      list match {
+        case Nil => -1
+        case x :: xs => if (ord.equiv(value, x)) {
+          elements.indexOf(value)
+        } else {
+          searchElement(xs, value)
+        }
+      }
+    }
+
+    searchElement(elements, value)(ord)
+  }
+}
