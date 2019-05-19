eea65f19ab54efa34309419e3636e3e0e331a1ef
maria ra <mariia.rabyk@gmail.com>
2019-05-19 17:59:02 +0300

    Added interpolation search

---
+++ src/main/scala/ua/com/searching/InterpolationSearch.scala
@@ -0,0 +1,30 @@
+package ua.com.searching
+
+import scala.annotation.tailrec
+
+object InterpolationSearch {
+
+  def search(elements: List[Int], value: Int): Int = {
+
+    @tailrec
+    def recursivelySearch(start: Int, end: Int): Option[Int] = {
+
+      if (start <= end && value >= elements(start) && value <= elements(end)) {
+        if (start == end) {
+          if (elements(start) == value) start else -1
+        }
+
+        val position = start + (((end-start) / (elements(end) - elements(start)))*(value - elements(start)))
+        position.toInt match {
+          case _ if end < start => None
+          case middle if elements(middle) == value => Some(middle)
+          case middle if value < elements(middle) => recursivelySearch(start, middle - 1)
+          case middle if value > elements(middle) => recursivelySearch(middle + 1, end)
+        }
+      } else {
+        None
+      }
+    }
+    recursivelySearch(0, elements.length - 1).getOrElse(-1)
+  }
+}
